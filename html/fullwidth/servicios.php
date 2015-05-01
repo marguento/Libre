270e30665adec5f5d8dfa6d0d597d3980dd23fa3
marcosmtz <marcosmtz@gmail.com>
2015-04-30 23:45:40 -0700

    Edit Footer PHP

--- html/fullwidth/servicios.php
+++ html/fullwidth/servicios.php
@@ -526,64 +526,7 @@
 <div class="clearfix"></div>
 
 
-<!-- COPIAR FOOTER DESDE AQUI -->
-<div class="footer1">
-	<div class="container">
-    	<div class="one_fourth">	
-		    <ul class="faddress">
-		    	<li style="padding-top: 0px; padding-bottom: 0px;"><img src="images/logonew2.png" alt="" width="50%"/></li>
-		        <li style="padding-top: 0px; padding-bottom: 0px;"><i class="fa fa-map-marker fa-lg"></i>&nbsp; Ctra. Libre Tijuana-Rosarito Km. 9 y 1/2 #27<br>Aguaje de la Tuna, Tijuana, BC 22490.</li>
-		        <li style="padding-top: 0px; padding-bottom: 0px;"><i class="fa fa-phone"></i>&nbsp; +52 (664) 637.1711</li>
-		        <li style="padding-top: 0px; padding-bottom: 0px;"><i class="fa fa-phone"></i>&nbsp; +1 (619) 694.1002</li>
-		        <li style="padding-top: 0px; padding-bottom: 0px;"><a href="contacto@libredeadicciones.com"><i class="fa fa-envelope"></i> contacto@libredeadicciones.com</a></li>
-			</ul>
-		</div><!-- end address -->
-    
-	    <div class="one_fourth">
-	    	<div class="frposts">
-    			<h3 class="lmb nocaps">Posts Recientes</h3>
-		        <ul>
-		            <li><a href="#"><i class="fa fa-caret-right"></i> Consectetuer adipiscn endisse</a></li>
-		            <li><a href="#"><i class="fa fa-caret-right"></i> Adipiscing luctus nisl magna</a></li>
-		            <li><a href="#"><i class="fa fa-caret-right"></i> 5 Plus Diffrent Header Designs</a></li>
-		            <li><a href="#"><i class="fa fa-caret-right"></i> Aliquam rhoncus, venenatis quis</a></li>
-		        </ul>
-        
-    		</div>
-    	</div>
-    
-    	<div class="one_fourth">
-    		<div class="twitter_feed">
-    			<h3 class="nocaps">Tweets Recientes</h3>
-			    <div class="left"><i class="fa fa-twitter fa-lg"></i></div>
-			    <div class="right">
-			    	<a href="https://twitter.com/gsrthemes9" target="_blank">gsrthemes9</a>: Libre de adicciones<br />
-			        <a href="#" class="small">.9 days ago</a> .
-			        <a href="#" class="small">reply</a> .
-			        <a href="#" class="small">retweet</a> .
-			        <a href="#" class="small">favorite</a>
-			   	</div>
-			        
-			    <div class="clearfix margin_top3"></div>    
-			        <div class="left"><i class="fa fa-twitter fa-lg"></i></div>
-			        <div class="right"><a href="https://twitter.com/gsrthemes9" target="_blank">Libre de adicciones<br />
-			        <a href="#" class="small">.10 days ago</a> .
-			        <a href="#" class="small">reply</a> .
-			        <a href="#" class="small">retweet</a> .
-			        <a href="#" class="small">favorite</a>
-			    </div>    
-			</div>
-    	</div>
-    
-    	<div class="one_fourth last">
-    		<div class="siteinfo">
-    			<h3 class="lmb nocaps">Quienes Somos</h3>
-        			<p>Somos un Centro de rehabilitación y una clinica especializada en el tratamiento de adicciones. Con un modelo de tratamiento integral y un enfoque sistemático y profesional</p>
-        		<br />
-			</div>
-   		</div>
-	</div>
-</div><!-- HASTA AQUI -->
+<?php include 'footer.php';?>
 
 <div class="clearfix"></div>
 
